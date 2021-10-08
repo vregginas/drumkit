b7145c8209f8e31d63bfc49c789c228fa840b1ee
vregginas <72319403+vregginas@users.noreply.github.com>
2021-10-08 21:49:21 +0300

    Update drums.js

--- drums.js
+++ drums.js
@@ -9,48 +9,48 @@ window.addEventListener('keydown',(e)=>{
 });
 
 document.getElementById('1').addEventListener("click",()=>{
-    var audio1=new Audio("sounds/clap.wav");
+    var audio1=new Audio("clap.wav");
     audio1.currentTime=0;
     audio1.play();
    
 });
 document.getElementById('2').addEventListener("click",()=>{
-    var audio2=new Audio("sounds/hihat.wav");
+    var audio2=new Audio("hihat.wav");
     audio2.currentTime=0;
     audio2.play();
 });
 document.getElementById('3').addEventListener("click",()=>{
-    var audio3=new Audio("sounds/kick.wav");
+    var audio3=new Audio("kick.wav");
     audio3.currentTime=0;
     audio3.play();
 });
 document.getElementById('4').addEventListener("click",()=>{
-    var audio4=new Audio("sounds/openhat.wav");
+    var audio4=new Audio("openhat.wav");
     audio4.currentTime=0;
     audio4.play();
 });
 document.getElementById('5').addEventListener("click",()=>{
-    var audio5=new Audio("sounds/boom.wav");
+    var audio5=new Audio("boom.wav");
     audio5.currentTime=0;
     audio5.play();
 });
 document.getElementById('6').addEventListener("click",()=>{
-    var audio6=new Audio("sounds/ride.wav");
+    var audio6=new Audio("ride.wav");
     audio6.currentTime=0;
     audio6.play();
 });
 document.getElementById('7').addEventListener("click",()=>{
-    var audio7=new Audio("sounds/snare.wav");
+    var audio7=new Audio("snare.wav");
     audio7.currentTime=0;
     audio7.play();
 });
 document.getElementById('8').addEventListener("click",()=>{
-    var audio8=new Audio("sounds/tom.wav");
+    var audio8=new Audio("tom.wav");
     audio8.currentTime=0;
     audio8.play();
 });
 document.getElementById('9').addEventListener("click",()=>{
-    var audio9=new Audio("sounds/tink.wav");
+    var audio9=new Audio("tink.wav");
     audio9.currentTime=0;
     audio9.play();
 });
